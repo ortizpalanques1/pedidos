63eebbe57e77ada82fc7d41cbc2655459f18fb90
ortizpalanques1 <marco.ortizpalanques@gmail.com>
2021-08-14 10:34:34 +0200

    Creation of the tab to write (create) products

--- app.R
+++ app.R
@@ -8,10 +8,18 @@ library(knitr)
 ## Functions and variables
 title <- tags$a(href='http://www.multilimpiezas.com/',
                 tags$img(src = "multilogo.png", height = '50', width = '77'),'Multilimpiezas')
+### Fecha
 fecha <- Sys.time()
 year <- format(as.Date(fecha, format="%Y-%m-%d"),"%Y")
 month <- format(as.Date(fecha, format="%Y-%m-%d"),"%m")
 day <- format(as.Date(fecha, format="%Y-%m-%d"),"%d")
+key1 <- paste0(as.character(year),as.character(month),as.character(day))
+print(key1)
+
+### Nuevo Producto
+nuevoProducto <- c("PROVEEDOR","CODIGO","PRODUCTO","PRECIO","TIPO")
+
+### Función Cargador
 source(file="opcionesMongoDB.R", local=T)[1]
 # options(mongodb = list(
 #     "host" = "qem.rodsc.mongodb.net",
@@ -29,6 +37,23 @@ cargador <- function(x,y){
                   options()$mongodb$host,
                   databaseName))
 }
+
+guardarNuevoProducto<- function(data) {
+  # Connect to the database
+  db <- mongo(collection = "productos",
+              url = sprintf(
+                "mongodb+srv://%s:%s@%s/%s?retryWrites=true&w=majority",
+                options()$mongodb$username,
+                options()$mongodb$password,
+                options()$mongodb$host,
+                "ISO"
+              ),
+              options = ssl_options(weak_cert_validation = TRUE))
+  # Insert the data into the mongo collection as a data.frame
+  data <- as.data.frame(t(data))
+  db$insert(data)
+}
+
 ## Load relevant data
 cargador("ISO","productos")
 productos <- db$find()
@@ -40,10 +65,10 @@ nombreCompleto <- paste(trabajadores$Nombres, trabajadores$Apellido_1, sep=" ")
 cargador("ISO","implantacion")
 implantacion <- db$find()
 
+## Load Multi image
 img1_path <- "C://Users/ortiz/OneDrive/pedidos/www/multilogo.png"
 img1 <- readPNG(img1_path, native = TRUE, info = TRUE)
 attr(img1, "info")
-
 
 ## Initial data frame
 pedidoTabla<- NULL
@@ -52,40 +77,76 @@ pedidoTabla<- NULL
 shinyApp(
   ui <-  dashboardPage(title = "PEDIDOS",
       dashboardHeader(title=title,titleWidth = 300),
-      dashboardSidebar(disable = TRUE ),
+      dashboardSidebar(
+        sidebarMenu(
+          menuItem("Pedido", tabName = "pedido", icon = icon("clipboard-list")),
+          menuItem("Nuevo Item", tabName = "nuevoItem", icon = icon("id-badge")))
+      ),
       dashboardBody(
-        fluidRow(
-          div( name = "hacerPedido",
-            column(
-              width = 3,
-              selectInput("supplier",
-                          "Proveedor",
-                          choices = levels(as.factor(productos$PROVEEDOR))),
-              uiOutput("secondSelection"),
-              numericInput("quantity", "Cantidad", 1,
-                           min = 1, max = 100),
-              actionButton("add","Añadir",icon = icon("plus-square")),
-              numericInput(inputId = "row.selection", label = "Selecciona el producto a borrar", min = 1, max = 100, value = ""),
-              actionButton(inputId = "delete", label = "Borrar", icon = icon("minus"))
+        tabItems( 
+          tabItem( tabName = "pedido",
+            fluidRow(
+              div( name = "hacerPedido",
+                column(
+                  width = 3,
+                  selectInput("supplier",
+                              "Proveedor",
+                              choices = levels(as.factor(productos$PROVEEDOR))),
+                  uiOutput("secondSelection"),
+                  numericInput("quantity", "Cantidad", 1,
+                               min = 1, max = 100),
+                  actionButton("add","Añadir",icon = icon("plus-square")),
+                  numericInput(inputId = "row.selection", label = "Selecciona el producto a borrar", min = 1, max = 100, value = ""),
+                  actionButton(inputId = "delete", label = "Borrar", icon = icon("minus"))
+              )
+             ),
+             div( name = "detallePedido",
+               column(
+                 width = 3,
+                 selectInput("direccion", "Direccion de envío", choices = implantacion$Direccion),
+                 selectInput("contacto","Responsable", choices = nombreCompleto),
+                 numericInput("desde","Hora Inicial de Recepción",8,min = 8, max = 16),
+                 numericInput("hasta","Hora Final de Recepción", 8, min = 8, max = 20),
+                 downloadButton("report", "Hacer Pedido")
+               )
+             ),
+             div( name = "hojaPedido", 
+               column(
+                 width = 6,
+                 DT::dataTableOutput("table")
+               )
+             ) 
+            )
+          ),
+          tabItem(tabName = "nuevoItem",
+            fluidRow(
+              div( id ="item",
+                   column(
+                     width = 3,
+                     selectInput("PROVEEDOR",
+                                 "Proveedor",
+                                 choices = levels(as.factor(productos$PROVEEDOR))),
+                     textInput("PRODUCTO",
+                               "Producto",
+                               value="",
+                               placeholder = "Nombre del producto"),
+                     textInput("CODIGO",
+                             "Código",
+                             value="",
+                             placeholder = "Código según Proveedor"),
+                     numericInput("PRECIO",
+                                  "Precio",
+                                  0),
+                     textInput("TIPO",
+                               "Tipo",
+                               value="",
+                               placeholder = "Puede ir vacío"),
+                     actionButton("enviar", "Enviar Datos", class = "btn-primary")
+                    ),
+              )
+            )
           )
-         ),
-         div( name = "detallePedido",
-           column(
-             width = 3,
-             selectInput("direccion", "Direccion de envío", choices = implantacion$Direccion),
-             selectInput("contacto","Responsable", choices = nombreCompleto),
-             numericInput("desde","Hora Inicial de Recepción",8,min = 8, max = 16),
-             numericInput("hasta","Hora Final de Recepción", 8, min = 8, max = 20),
-             downloadButton("report", "Hacer Pedido")
-           )
-         ),
-         div( name = "hojaPedido", 
-           column(
-             width = 6,
-             DT::dataTableOutput("table")
-           )
-         ) 
-        )
+        )      
       )
     ),
   server = function(input, output) {
@@ -151,5 +212,16 @@ shinyApp(
         )
       }
     )
+    ########################################################################################################
+    # TAB: nuevoItem
+    formData <- reactive({
+      data <- sapply(nuevoProducto, function(x) input[[x]])
+      data
+    })
+    
+    observeEvent(input$enviar, {
+      guardarNuevoProducto(formData())
+    })
+    
   }
 )
